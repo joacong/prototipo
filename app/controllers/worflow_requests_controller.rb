40a3e0d64f273c342d75fba27460e6c4c19b0d3e
Prelang Builder <builder@prelang.com>
2015-08-26 19:24:03 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/worflow_requests_controller.rb
+++ app/controllers/worflow_requests_controller.rb
@@ -1,4 +1,8 @@
 class WorflowRequestsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_worflow_request, only: [:show, :edit, :update, :destroy]
 
   # GET /worflow_requests
