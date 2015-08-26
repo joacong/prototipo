28a72c67ea030c2e7ce44c4c5c189f1ba199da36
Prelang Builder <builder@prelang.com>
2015-08-26 19:24:27 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'ActiveWorkflowStep' to allow for create, update

--- app/admin/active_workflow_step.rb
+++ app/admin/active_workflow_step.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register ActiveWorkflowStep do
+  permit_params :number
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
