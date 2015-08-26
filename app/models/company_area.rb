5812e44c44dd3f3d115e33d913641e9cf36f0f20
Joaquin Nuñez <nunezg.ja@gmail.com>
2015-08-26 17:09:12 -0300

    Add company area parent

--- app/models/company_area.rb
+++ app/models/company_area.rb
@@ -1,4 +1,12 @@
 class CompanyArea < ActiveRecord::Base
   belongs_to :company_area
   has_many :users
+
+  def parent
+  	CompanyArea.find_by_id company_area_id
+  end
+
+  def parent_name
+  	parent.try(:name)
+  end
 end
