8f501fb3db383e246e7c529aeb0a6d9f6acdc156
Prelang Builder <builder@prelang.com>
2015-08-26 19:24:08 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'WorflowRequest' to allow for create, update

--- app/admin/worflow_request.rb
+++ app/admin/worflow_request.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register WorflowRequest do
+  permit_params 
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
